20ffa25f4c0afc0c63773d2881695a2be45ac389
Deepak <deepakjat4499@gmail.com>
2026-02-19 11:11:23 +0530

    somthing 1

---
+++ Revision/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+   res.send("hello this is root");
+});
+
+app.listen(3000, ()=> {
+    console.log("app is lestening on prot 3000");
+;})
